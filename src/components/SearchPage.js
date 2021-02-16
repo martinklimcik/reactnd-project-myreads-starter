3af689b8747e266d3cb9837cc95b1579b7998ef1
Martin Klimcik <martin.klimcik@gmail.com>
2021-02-16 11:38:15 +0100

    feat: SearchPage working
    Input on SearchPage is now working
    Split SearchPage into components

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,21 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
+import BookGrid from "./BookGrid";
+
+const SearchInput = (props) => {
+  return (
+    <div className="search-books-bar">
+      <Link to="/">
+        <button className="close-search">Close</button>
+      </Link>
+      <div className="search-books-input-wrapper">
+        <input
+          type="text"
+          placeholder="Search by title or author"
+          value={props.searchQuery}
+          onChange={props.handleInput}
+        />
+      </div>
+    </div>
+  );
+};
+
+const SearchResults = (props) => {
+  return (
+    <div className="search-books-results">
+      <BookGrid books={props.books} />
+    </div>
+  );
+};
 
 class SearchPage extends React.Component {
+  state = { searchQuery: "", foundBooks: [] };
+
+  handleInput = (event) => {
+    // TODO: make sure deleting input deletes all found - query arrives later and overwrites setState after first if
+    const query = event.target.value.trim();
+    this.setState({
+      searchQuery: query,
+    });
+    if (query === "") {
+      this.setState({ foundBooks: [] });
+    } else {
+      BooksAPI.search(query).then((data) => {
+        if (Array.isArray(data)) {
+          this.setState({ foundBooks: data });
+        } else {
+          this.setState({ foundBooks: [] });
+        }
+      });
+    }
+  };
+
   render() {
     return (
       <div className="search-books">
-        <div className="search-books-bar">
-          <Link to="/">
-            <button className="close-search">Close</button>
-          </Link>
-          <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid" />
-        </div>
+        <SearchInput searchQuery={this.state.searchQuery} />
+        <SearchResults books={this.state.foundBooks} />
       </div>
     );
   }
